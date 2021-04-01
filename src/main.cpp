2a29ed7bdb706d92744c3d54242c2d045fe14a1b
Jeremiah Korreck <korreckj328@gmail.com>
2021-04-01 10:38:51 -0400

    3 Level LOD model Example

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,44 @@
 // Jeremiah Korreck
 
 #include <osg/MatrixTransform>
-#include <osg/Switch>
+#include <osg/LOD>
+#include <osgUtil/Simplifier>
 #include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
 
 int main() {
 
     // load the Models
-    osg::ref_ptr<osg::Node> cessna = osgDB::readNodeFile("Resources/cessna.osg");
-    osg::ref_ptr<osg::Node> cessna_onfire = osgDB::readNodeFile("Resources/cessnafire.osg");
+    osg::ref_ptr<osg::Node> cessnaL3 = osgDB::readNodeFile("Resources/cessna.osg");
+    osg::ref_ptr<osg::Node> cessnaL2 = dynamic_cast<osg::Node*>(cessnaL3->clone(osg::CopyOp::DEEP_COPY_ALL));
+    osg::ref_ptr<osg::Node> cessnaL1 = dynamic_cast<osg::Node*>(cessnaL3->clone(osg::CopyOp::DEEP_COPY_ALL));
     
     // create transforms for each of the models I want to render in the scene
     osg::ref_ptr<osg::MatrixTransform> cessnaTransform1 = new osg::MatrixTransform;
     
-    // create a switch node
-    osg::ref_ptr<osg::Switch> cessnaModelSwitch = new osg::Switch;
-    cessnaModelSwitch->addChild(cessna.get(), false);
-    cessnaModelSwitch->addChild(cessna_onfire.get(), true);
-    
-    // add the models to the transforms, note the cessnaTransforms are sharing their leaf node
-    cessnaTransform1->addChild(cessnaModelSwitch.get());
-    
+    // create simplifier
+    osgUtil::Simplifier simplifier;
+    simplifier.setSampleRatio(0.5);
+
+    // run the second model thru the simplifier
+    cessnaL2->accept(simplifier);
+
+    // set the simplifier to a smaller ratio and run L1
+    simplifier.setSampleRatio(0.1);
+    cessnaL1->accept(simplifier);
+
+    // create the LOD node
+    osg::ref_ptr<osg::LOD> cessnaLOD = new osg::LOD;
+    cessnaLOD->addChild(cessnaL1.get(), 200.0f, FLT_MAX);
+    cessnaLOD->addChild(cessnaL2.get(), 50.0f, 200.0f);
+    cessnaLOD->addChild(cessnaL3.get(), 0.0f, 50.0f);
+
+    // add the LOD node to the models transform
+    cessnaTransform1->addChild(cessnaLOD.get());
+
     // add the transforms to the root node of the scene
     osg::ref_ptr<osg::Group> root = new osg::Group;
     root->addChild(cessnaTransform1.get());
-    
-    // switchnode set value
-    cessnaModelSwitch->setValue(0, true);
-    cessnaModelSwitch->setValue(1, false);
     
     //create the viewer and then render the root
     osgViewer::Viewer viewer;
