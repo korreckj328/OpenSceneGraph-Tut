473686609bc03b5257bf70bb406ac68383308a87
Jeremiah Korreck <korreckj328@gmail.com>
2021-04-01 13:54:25 -0400

    changed to glider model and disabled and enabled lighting.

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,31 @@
 int main() {
 
     // load the Models
-    osg::ref_ptr<osg::Node> cessna = osgDB::readNodeFile("Resources/cessna.osg");
+    osg::ref_ptr<osg::Node> glider = osgDB::readNodeFile("Resources/glider.osg");
     
     // create transforms for each of the models I want to render in the scene
-    osg::ref_ptr<osg::MatrixTransform> cessnaTransformRight = new osg::MatrixTransform;
-    osg::ref_ptr<osg::MatrixTransform> cessnaTransformLeft = new osg::MatrixTransform;
+    osg::ref_ptr<osg::MatrixTransform> gliderTransformRight = new osg::MatrixTransform;
+    osg::ref_ptr<osg::MatrixTransform> gliderTransformLeft = new osg::MatrixTransform;
     
-    cessnaTransformRight->setMatrix(osg::Matrix::translate(25.0f, 0.0f, 0.0f));
-    cessnaTransformLeft->setMatrix(osg::Matrix::translate(-25.0f, 0.0f, 0.0f));
+    gliderTransformRight->setMatrix(osg::Matrix::translate(0.5f, 0.0f, 0.0f));
+    gliderTransformLeft->setMatrix(osg::Matrix::translate(-0.5f, 0.0f, 0.0f));
     
-    cessnaTransformRight->addChild(cessna.get());
-    cessnaTransformLeft->addChild(cessna.get());
+    gliderTransformRight->addChild(glider.get());
+    gliderTransformLeft->addChild(glider.get());
 
     osg::ref_ptr<osg::PolygonMode> pm = new osg::PolygonMode;
-    pm->setMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE);
-    cessnaTransformRight->getOrCreateStateSet()->setAttribute(pm.get());
+    pm->setMode(osg::PolygonMode::FRONT, osg::PolygonMode::FILL);
+    gliderTransformRight->getOrCreateStateSet()->setAttribute(pm.get());
 
     // add the transforms to the root node of the scene
     osg::ref_ptr<osg::Group> root = new osg::Group;
-    root->addChild(cessnaTransformRight.get());
-    root->addChild(cessnaTransformLeft.get());
+    root->addChild(gliderTransformRight.get());
+    root->addChild(gliderTransformLeft.get());
+    
+    // add lighting attributes
+    gliderTransformRight->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
+    gliderTransformLeft->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::OFF|osg::StateAttribute::PROTECTED);
+    root->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::ON|osg::StateAttribute::OVERRIDE );
     
     //create the viewer and then render the root
     osgViewer::Viewer viewer;
