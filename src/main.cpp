bc241527925179dd12b510d5fa93d805afd03dcb
Jeremiah Korreck <korreckj328@gmail.com>
2021-03-31 22:45:08 -0400

    Added Three transforms to load the cessna and cow models

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,37 @@
 // open scene graph tutorial
 // Jeremiah Korreck
 
-#include <osg/Geometry>
-#include <osg/Geode>
-#include <osgUtil/Tessellator>
-#include <osg/TriangleFunctor>
+#include <osg/MatrixTransform>
+#include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
-#include <iostream>
-#include "PrintableVec3.h"
-
-struct FaceCollector
-{
-    void operator() (const osg::Vec3 v1, const osg::Vec3 v2, 
-                     const osg::Vec3 v3)
-    {
-        PrintableVec3 pv1 = PrintableVec3(v1.x(), v1.y(), v1.z());
-        PrintableVec3 pv2 = PrintableVec3(v2.x(), v2.y(), v2.z());
-        PrintableVec3 pv3 = PrintableVec3(v3.x(), v3.y(), v3.z());
-        std::cout << "Face vertices: " << pv1 << "; " << pv2 << "; " << pv3 << std::endl;
-    }
-};
 
 int main() {
 
-    // create the vertices
-    osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
-    vertices->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
-    vertices->push_back(osg::Vec3(0.0f, 0.0f, 1.0f));
-    vertices->push_back(osg::Vec3(1.0f, 0.0f, 0.0f));
-    vertices->push_back(osg::Vec3(1.0f, 0.0f, 1.5f));
-    vertices->push_back(osg::Vec3(2.0f, 0.0f, 0.0f));
-    vertices->push_back(osg::Vec3(2.0f, 0.0f, 1.0f));
-    vertices->push_back(osg::Vec3(3.0f, 0.0f, 0.0f));
-    vertices->push_back(osg::Vec3(3.0f, 0.0f, 1.5f));
-    vertices->push_back(osg::Vec3(4.0f, 0.0f, 0.0f));
-    vertices->push_back(osg::Vec3(4.0f, 0.0f, 1.0f));
-
-    //create the normals
-    osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
-    normals->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
-
-    //create geometry
-
-    osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
-    geom->setVertexArray(vertices.get());
-    geom->setNormalArray(normals.get());
-    geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
-    geom->addPrimitiveSet(new osg::DrawArrays(GL_QUAD_STRIP, 0, 10));
-
-    osg::TriangleFunctor<FaceCollector> functor;
-    geom->accept( functor );
-
-    // add the geometry to a geode and create the scene root
-    osg::ref_ptr<osg::Geode> root = new osg::Geode;
-    root->addDrawable(geom.get());
-
+    // load the Models
+    osg::ref_ptr<osg::Node> cessna = osgDB::readNodeFile("Resources/cessna.osg");
+    osg::ref_ptr<osg::Node> cow = osgDB::readNodeFile("Resources/cow.osg");
+    
+    // create transforms for each of the models I want to render in the scene
+    osg::ref_ptr<osg::MatrixTransform> cessnaTransform1 = new osg::MatrixTransform;
+    osg::ref_ptr<osg::MatrixTransform> cessnaTransform2 = new osg::MatrixTransform;
+    osg::ref_ptr<osg::MatrixTransform> cowTransform = new osg::MatrixTransform;
+    
+    // from the perspective of the camera move the cessnas right and left and the cow backwards
+    cessnaTransform1->setMatrix(osg::Matrix::translate(-25.0f, 0.0f, 0.0f));
+    cessnaTransform2->setMatrix(osg::Matrix::translate(25.0f, 0.0f, 0.0f));
+    cowTransform->setMatrix(osg::Matrix::translate(0.0f, 25.0f, 0.0f));
+    
+    // add the models to the transforms, note the cessnaTransforms are sharing their leaf node
+    cessnaTransform1->addChild(cessna.get());
+    cessnaTransform2->addChild(cessna.get());
+    cowTransform->addChild(cow.get());
+    
+    // add the transforms to the root node of the scene
+    osg::ref_ptr<osg::Group> root = new osg::Group;
+    root->addChild(cessnaTransform1.get());
+    root->addChild(cessnaTransform2.get());
+    root->addChild(cowTransform.get());
+    
     //create the viewer and then render the root
     osgViewer::Viewer viewer;
     viewer.setSceneData(root.get());
