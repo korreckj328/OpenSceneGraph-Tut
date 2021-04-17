452ebe15f5ad9331c53331488944e63e7bded20a
Jeremiah Korreck <korreckj328@gmail.com>
2021-04-17 14:58:43 -0400

    Fixed Typo that was autofixed on mac but presented a bug on linux.  Tested multi-sample on a intel hd 4400.

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,13 @@ int main() {
     // set the number of multisamples  2,4, or 6
     // rpi 4 only accepts 2, 4
     // apple M1 os 11.2.3 supports 6
+    // linux intel 4400HD supports 6
+    // this seems to indicate that anything recent and modern will support 6
     
     osg::DisplaySettings::instance()->setNumMultiSamples(6);
 
     osg::ref_ptr<osg::Node> cessna = osgDB::readNodeFile("Resources/cessna.osg");
-    osg::ref_ptr<osg::Node> cessnaFire = osgDB::readNodeFile("Resources/cessnaFire.osg");
+    osg::ref_ptr<osg::Node> cessnaFire = osgDB::readNodeFile("Resources/cessnafire.osg");
 
     osg::ref_ptr<osg::Switch> cessnaSwitch = new osg::Switch;
     cessnaSwitch->addChild(cessna.get(), false);
